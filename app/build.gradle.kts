ab50b287bac46164a513141cdbb0eae1186ce98b
Xsufu <aluno.macarrao@gmail.com>
2022-10-19 18:24:30 +0700

    Добавлена зависимость для аннтоаций Room

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,7 @@ dependencies {
     implementation("androidx.navigation:navigation-fragment-ktx:2.5.2")
     implementation("androidx.navigation:navigation-ui-ktx:2.5.2")
 
+    annotationProcessor("androidx.room:room-compiler:2.4.3")
     implementation("androidx.room:room-runtime:2.4.3")
     kapt("androidx.room:room-compiler:2.4.3")
     implementation("androidx.room:room-ktx:2.4.3")
