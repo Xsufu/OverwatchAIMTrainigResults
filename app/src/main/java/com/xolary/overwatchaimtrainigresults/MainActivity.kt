9a37a4742578a4ba6183b2cb698577fa3ee597b6
Xsufu <aluno.macarrao@gmail.com>
2022-10-19 17:59:18 +0700

    Initial commit

---
+++ app/src/main/java/com/xolary/overwatchaimtrainigresults/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.xolary.overwatchaimtrainigresults
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
